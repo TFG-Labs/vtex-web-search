44045537156e2f0e88e2be16f34ff5f29432879e
Shaheen Karodia <karodia.shaheen@gmail.com>
2022-07-22 17:45:25 +0200

    Deleted unused styles

--- react/components/Autocomplete/components/CustomListItem/CustomListItem.tsx
+++ react/components/Autocomplete/components/CustomListItem/CustomListItem.tsx
@@ -2,8 +2,6 @@
 import React, { FC } from 'react'
 import { Link } from 'vtex.render-runtime'
 
-import styles from './styles.css'
-
 interface CustomListItemProps {
   product: Product
   onClick: () => void
@@ -23,25 +21,10 @@ const CustomListItem: FC<CustomListItemProps> = ({ product, onClick }) => {
         className="no-underline"
         onClick={onClick}
       >
-        <article
-          className={`${styles.element} flex flex-row justify-start items-center pa3 bg-animate hover-bg-light-gray`}
-        >
-          <div className={`${styles.imageContainer} h3`}>
-            <img
-              className={`${styles.image} h-100 w-auto mw-none`}
-              src={sku?.image?.imageUrl}
-            />
-          </div>
-          <div
-            className={`${styles.information} flex flex-column justify-between items-start ml4`}
-          >
-            <div className={styles.productNameContainer}>
-              <span className={`${styles.productBrand} f5 c-on-base`}>
-                {product.productName}
-              </span>
-            </div>
-          </div>
-        </article>
+        <div>
+          <img src={sku?.image?.imageUrl} />
+          {product.productName}
+        </div>
       </Link>
     </div>
   )
